5378995f2e9dff39e722646fdd9ff215aeee87fa
DennisGoss99 <dennisgossler98@gmail.com>
2022-06-25 11:41:35 +0200

    add diagram

--- Application/src/main/kotlin/cga/exercise/game/ParallelTest.kt
+++ Application/src/main/kotlin/cga/exercise/game/ParallelTest.kt
@@ -3,29 +3,51 @@ package cga.exercise.game
 import cga.framework.printlnTimeMillis
 import cga.framework.printlnTimeNano
 import kotlinx.coroutines.*
-import java.util.*
 import kotlin.concurrent.timer
+import kotlin.random.Random
+
 import kotlin.system.measureNanoTime
 
 fun main() {
 
     runBlocking {
+        repeat(10){
 
-        printlnTimeNano {
-            val jobs = mutableListOf<Job>()
+            var i = 23123L
+            val l = MutableList<Int>(1000000){ Random(i++).nextInt(0,10000000)}
+            i = 23123L
+            val l2 = MutableList<Int>(1000000){Random(i++).nextInt(0,10000000)}
+//
+//
+//            printlnTimeNano {
+//
+//                val jobs = mutableListOf<Job>()
+//
+//                repeat(1){
+//                    jobs.add( GlobalScope.launch {
+//                        l.sort()
+//                    })
+//                }
+//
+//                jobs.joinAll()
+//            }
 
-            repeat(4){
-                jobs.add( GlobalScope.launch {
-
-                })
+            printlnTimeNano {
+                l2.sort()
             }
 
-            jobs.joinAll()
         }
 
+
+//        printlnTimeNano {
+//            val jobs = mutableListOf<Job>()
+//            repeat(1){
+//                jobs.add( GlobalScope.launch {
+//                })
+//            }
+//
+//            jobs.joinAll()
+//        }
+
     }
-
-
-
-
 }
